6cd0573bb9f36622877e484a9bc76cf98f36956f
Saditud <p.bregeard57@gmail.com>
2022-07-08 22:02:06 +0200

    matrix upload

--- canvas/matrix/vue/main.js
+++ canvas/matrix/vue/main.js
@@ -3,76 +3,60 @@ const canvas = document.getElementById("canvas1");
 const context = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-const ArrayParti = [];
-const divElement = document.getElementById("brick");
-let divMeasurements = divElement.getBoundingClientRect();
-let div = {
-    x: divMeasurements.left,
-    y: divMeasurements.top,
-    width: divMeasurements.width,
-    height: divMeasurements.height,
-};
-class Particule {
-    constructor(x, y, size = Math.random() * 7 + 1, weight = Math.random() * 3 + 1, directionX = -1) {
-        this.x = x;
-        this.y = y;
-        this.size = size;
-        this.weight = weight;
-        this.directionX = directionX;
+const chains = [];
+class Chain {
+    constructor() {
+        this.Y = Math.random() * canvas.height;
+        this.X = Math.random() * canvas.width;
+    }
+    getLetter() {
+        // http://www.rikai.com/library/kanjitables/kanji_codes.unicode.shtml
+        /*
+            // Letters
+             if(Math.round(Math.random()) == 0)
+              return String.fromCharCode(Math.random() * 26 + 97)
+            else
+              return String.fromCharCode(Math.random() * 26 + 65)
+        
+            // Binary
+            return `${Math.round(Math.random())}`;
+         */
+        // Japanese
+        return String.fromCharCode(Math.random() * 86 + 0x30a1);
     }
     update() {
-        if (this.y > canvas.height || this.x < 0) {
-            this.y = 0 - this.size;
-            this.weight = Math.random() * 3 + 1;
-            this.x = Math.random() * canvas.width * 1.3;
+        if (this.Y >= canvas.height) {
+            this.Y = 0;
+            this.X = Math.random() * canvas.width;
         }
-        this.weight += 0.01;
-        this.y += this.weight;
-        this.x += this.directionX;
-        if (this.x < div.x + div.width &&
-            this.x + this.size > div.x &&
-            this.y < div.y + div.height &&
-            this.y + this.size > div.y) {
-            this.y -= 3;
-            this.weight *= -0.3;
-        }
+        else
+            this.Y += 28;
     }
     draw() {
-        context.fillStyle = "blue";
-        context.beginPath();
-        context.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-        context.closePath();
-        context.fill();
+        context.font = '30px serif';
+        context.fillStyle = '#00c407';
+        context.fillText(this.getLetter(), this.X, this.Y);
     }
 }
-function init() {
-    while (ArrayParti.length !== 0)
-        ArrayParti.pop();
-    for (let i = 0; i < 100; i++)
-        ArrayParti.push(new Particule(Math.random() * canvas.width, Math.random() * canvas.height));
+function init(nbChains) {
+    for (let i = 0; i < nbChains; i++)
+        chains.push(new Chain());
+}
+function wait(delay) {
+    const time = new Date();
+    time.setMilliseconds(time.getMilliseconds() + delay);
+    while (time >= new Date()) { }
 }
 function animate() {
-    context.fillStyle = "rgba(255, 255, 255, 0.05)";
+    context.fillStyle = "rgba(0, 0, 0, 0.06)";
     context.fillRect(0, 0, canvas.width, canvas.height);
-    ArrayParti.forEach((element) => {
+    chains.forEach((element) => {
         element.update();
         element.draw();
     });
-    context.fillRect(div.x, div.y, div.width, div.height);
+    wait(10);
     requestAnimationFrame(animate);
 }
+init(20);
 animate();
-init();
-window.addEventListener("resize", () => {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    divMeasurements = divElement.getBoundingClientRect();
-    div = {
-        x: divMeasurements.left,
-        y: divMeasurements.top,
-        width: divMeasurements.width,
-        height: divMeasurements.height,
-    };
-    init();
-});
 //# sourceMappingURL=main.js.map
