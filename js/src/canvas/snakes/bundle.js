4056546fff56abba09c5af9082055e113900b7b4
Saditud <p.bregeard57@gmail.com>
2022-08-09 18:41:54 +0200

    ./send.sh

--- js/src/canvas/snakes/bundle.js
+++ js/src/canvas/snakes/bundle.js
@@ -1,5 +1,51 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 "use strict";
+const { createNoise2D } = require('simplex-noise');
+const canvas = document.getElementById('canvas1');
+const context = canvas.getContext('2d');
+const noise2D = createNoise2D();
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+class Snake {
+    constructor(color) {
+        this.color = color;
+        this.size = 6;
+        /*
+        public color = `#${Array(6).fill('')
+            .map(() => (Math.random() * 0xF << 0).toString(16))
+            .join('')}`; */
+        this.seed = Math.random() * 10000;
+    }
+    update(time) {
+        this.x = noise2D(time, this.seed) * (canvas.width / 2) + canvas.width / 2;
+        this.y = noise2D(time + 200, this.seed + 200) * (canvas.height / 2) + canvas.height / 2;
+    }
+    draw() {
+        context.fillStyle = this.color;
+        context.beginPath();
+        context.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        context.closePath();
+        context.fill();
+    }
+}
+const nbSnakes = 10;
+const Snakes = [];
+for (let i = 0; i < nbSnakes; i++)
+    Snakes.push(new Snake(`hsl(${Math.random() * 1000 + ((360 / nbSnakes) * i)}, 70%, 50%)`));
+let time = 0;
+const animate = () => {
+    Snakes.forEach((element) => {
+        element.update(time);
+        element.draw();
+    });
+    // time += 0.0005; // normal speed
+    time += 0.0009;
+    requestAnimationFrame(animate);
+};
+animate();
+
+},{"simplex-noise":2}],2:[function(require,module,exports){
+"use strict";
 /*
  * A fast javascript implementation of simplex noise by Jonas Wagner
 
@@ -470,50 +516,4 @@ function buildPermutationTable(random) {
 }
 exports.buildPermutationTable = buildPermutationTable;
 
-},{}],2:[function(require,module,exports){
-"use strict";
-const { createNoise2D } = require('simplex-noise');
-const canvas = document.getElementById('canvas1');
-const context = canvas.getContext('2d');
-const noise2D = createNoise2D();
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-class Snake {
-    constructor(color) {
-        this.color = color;
-        this.size = 6;
-        /*
-        public color = `#${Array(6).fill('')
-            .map(() => (Math.random() * 0xF << 0).toString(16))
-            .join('')}`; */
-        this.seed = Math.random() * 10000;
-    }
-    update(time) {
-        this.x = noise2D(time, this.seed) * (canvas.width / 2) + canvas.width / 2;
-        this.y = noise2D(time + 200, this.seed + 200) * (canvas.height / 2) + canvas.height / 2;
-    }
-    draw() {
-        context.fillStyle = this.color;
-        context.beginPath();
-        context.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-        context.closePath();
-        context.fill();
-    }
-}
-const nbSnakes = 10;
-const Snakes = [];
-for (let i = 0; i < nbSnakes; i++)
-    Snakes.push(new Snake(`hsl(${Math.random() * 1000 + ((360 / nbSnakes) * i)}, 70%, 50%)`));
-let time = 0;
-const animate = () => {
-    Snakes.forEach((element) => {
-        element.update(time);
-        element.draw();
-    });
-    // time += 0.0005; // normal speed
-    time += 0.0009;
-    requestAnimationFrame(animate);
-};
-animate();
-
-},{"simplex-noise":1}]},{},[2]);
+},{}]},{},[1]);
---
+++ main.ts
@@ -0,0 +1,5 @@
+export {};
+const canvas = <HTMLCanvasElement>document.getElementById('canvas1');
+const context = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
